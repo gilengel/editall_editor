7e892269e6be5f2b0a3e9b835b1b0f634f411936
Gil Engel <gil.engel@web.de>
2022-01-27 20:44:34 +0100

    Move editor code into subcrate

---
+++ rust_editor/src/interactive_element.rs
@@ -0,0 +1,18 @@
+use serde::{Serialize, Deserialize};
+
+use crate::{style::Style};
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
+pub enum InteractiveElementState {
+    Normal,
+    Hover,
+    Selected
+}
+
+pub trait InteractiveElement {
+    fn state(&self) -> InteractiveElementState;
+
+    fn set_state(&mut self, new_state: InteractiveElementState);
+
+    fn style(&self) -> &Style;
+}
